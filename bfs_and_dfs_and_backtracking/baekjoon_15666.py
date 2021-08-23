b18805c6d5e564b3b47a814c09c40c2ce6297511
OTKRyu <appn12@gmail.com>
2021-08-23 20:02:17 +0900

    0823

---
+++ bfs_and_dfs_and_backtracking/baekjoon_15666.py
@@ -0,0 +1,26 @@
+def per(step):
+    if step == m:
+        print(*result)
+    else:
+        if step == 0:
+            for i in range(len(nums)):
+                if check[nums[i]] != 0:
+                    result.append(nums[i])
+                    per(step+1)
+                    result.pop()
+        else:
+            for i in range(len(nums)):
+                if check[nums[i]] != 0 and result[-1] <= nums[i]:
+                    result.append(nums[i])
+                    per(step+1)
+                    result.pop()
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+check = [0 for i in range(10001)]
+for i in range(len(nums)):
+    check[nums[i]] = 1
+nums = list(set(nums))
+nums.sort()
+result = []
+per(0)
